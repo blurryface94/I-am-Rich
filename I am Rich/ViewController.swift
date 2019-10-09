ab196a304eb4ad4ca7e0fd552178162f953b4960
blurryface94 <alka.lizzy@gmail.com>
2019-10-09 20:20:05 +1100

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Alka Sebastian on 9/10/19.
+//  Copyright © 2019 Alka Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
